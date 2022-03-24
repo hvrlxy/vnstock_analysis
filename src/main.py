8d8dcbf28bfacf4a4f15f2b006bc6fe73de495f0
hvrlxy <hle5@gustavus.edu>
2022-03-23 20:27:44 -0500

    push sample dataset

--- src/main.py
+++ src/main.py
@@ -1,14 +1,23 @@
 from merge_data import *
 import pandas as pd
+from ta import add_all_ta_features
+from ta.utils import dropna
 
-stock_list = ['VND', 'MBB']
+
+stock_list = ['HPG', 'MSR', 'DIG', 'FOX', 'ORS', 'PVS', 'GAS', 'GEX', 'MBB'] #'ACV'
 start_date = '2017-01-01'
-end_date = '2022-02-01'
+end_date = '2028-02-01'
 
 final_df = pd.DataFrame()
 for stock_code in stock_list:
+    print(stock_code)
     stock_df = MergeData(stock_code, start_date, end_date).simple_price_df
     final_df = final_df.append(stock_df, ignore_index=True)
+    break
 
-# final_df.to_csv('../results/csvs/simplifies_df.csv')
-final_df.to_excel('../results/excels/simplifies_df.xlsx')
+mom_data = add_all_ta_features(final_df, open="open",
+                               high="high", low="low", close="close", volume="volume")
+
+
+mom_data.to_csv('../results/csvs/simplified_df.csv')
+mom_data.to_excel('../results/excels/simplified_df.xlsx')
